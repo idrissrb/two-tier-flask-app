70b05208928f9614552b345fff00b6f6ca161038
idriss <idrissrebah88@gmail.com>
2026-03-26 19:05:22 +0100

    Fix column name in init.sql to match app.py

--- init.sql
+++ init.sql
@@ -4,7 +4,7 @@ USE mydb;
 
 CREATE TABLE IF NOT EXISTS messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    content VARCHAR(255)
+    message VARCHAR(255) NOT NULL
 );
 
 INSERT INTO messages (content) VALUES ('Hello from DB!');
